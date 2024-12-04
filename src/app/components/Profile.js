87d249794ec9a7ee952309942d49a96d719b56bd
linangon <linangon>
2024-12-04 16:54:03 +0700

    profile

--- src/app/components/Profile.js
+++ src/app/components/Profile.js
@@ -1,2 +1,12 @@
 import React from "react";
-import drinkImage from ""
+
+export default function Profile(){
+    return (
+        <div className="bg-primary w-[350px] h-[250px] border-2 border-black rounded-2xl flex item-center justify-center m-5">
+            <img
+                src="/drinkImage.png" alt="drink"
+                className="w-auto h-auto"
+            ></img>
+        </div>
+    )
+}
