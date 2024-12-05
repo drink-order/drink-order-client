01c6e9300ef266433681b8c3526fbbfdb5fe7cf0
VVicheka <150057081+VVicheka@users.noreply.github.com>
2024-12-05 14:17:17 +0700

    add nav bar

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,26 +1,24 @@
 import React from "react";
-import { FaHome, FaWallet, FaCog, FaUser } from "react-icons/fa"; // Import React Icons
+import { FaHome, FaWallet, FaCog, FaUser } from "react-icons/fa";
 
 const NavBar = () => {
   const menuItems = [
-    { icon: <FaHome className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />, label: "Home", link: "/" },
-    { icon: <FaWallet className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />, label: "Wallet", link: "/wallet" },
-    { icon: <FaCog className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />, label: "Settings", link: "/settings" },
-    { icon: <FaUser className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />, label: "Profile", link: "/profile" },
+    { icon: <FaHome className="w-5 h-5 mb-2 text-gray2  group-hover:text-blue-600" />, label: "Home", link: "/" },
+    { icon: <FaWallet className="w-5 h-5 mb-2 text-gray2 group-hover:text-blue-600" />, label: "Wallet", link: "/wallet" },
+    { icon: <FaCog className="w-5 h-5 mb-2 text-gray2 group-hover:text-blue-600" />, label: "Settings", link: "/settings" },
+    { icon: <FaUser className="w-5 h-5 mb-2 text-gray2 group-hover:text-blue-600" />, label: "Profile", link: "/profile" },
   ];
 
   return (
-    <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+    <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200">
       <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
         {menuItems.map((item, idx) => (
-          <button key={idx} type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-            <a href={item.link} className="text-center">
-              {item.icon}
-              <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
-                {item.label}
-              </span>
-            </a>
-          </button>
+          <a key={idx} href={item.link} className="flex flex-col items-center justify-center px-5 hover:bg-gray-50 group">
+            {item.icon}
+            <span className="text-sm text-gray group-hover:text-blue-600">
+              {item.label}
+            </span>
+          </a>
         ))}
       </div>
     </div>
