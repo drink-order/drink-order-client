18ddd7a918fb566874f27a546b5e83ec9d7f2824
VVicheka <150057081+VVicheka@users.noreply.github.com>
2025-01-18 13:59:59 +0700

    fix before build

--- src/app/(dashboard)/admin/components/CategoryList.jsx
+++ src/app/(dashboard)/admin/components/CategoryList.jsx
@@ -1,41 +1,43 @@
-import React from 'react'
+import React from 'react';
 import Removebtn from './Removebtn';
 import Link from 'next/link';
 import { HiPencilAlt } from 'react-icons/hi';
 
-const getCategories = async() => {
+const getCategories = async () => {
   try {
     const res = await fetch("http://localhost:3000/admin/api/category", {
       cache: "no-store",
     });
 
-    if(!res.ok){
-      throw new Error("Failed to fetch categories")
+    if (!res.ok) {
+      throw new Error("Failed to fetch categories");
     }
-    return res.json();
+
+    const data = await res.json();
+    return data;
 
   } catch (error) {
     console.log("Error loading categories: ", error);
+    return { categories: [] }; // Return an empty array if there is an error
   }
 }
 
 export default async function CategoryList() {
-
   const { categories } = await getCategories();
 
   return (
     <div>
       {categories.map(c => (
-        <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+        <div key={c._id} className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
           <div>
-              <h2 className="font-bold text-lg">{c.idCategory}</h2>
-              <div>{c.nameCategory}</div>
+            <h2 className="font-bold text-lg">{c.idCategory}</h2>
+            <div>{c.nameCategory}</div>
           </div>
           <div className="flex gap-2">
-              <Removebtn id={c._id} />
-              <Link href={`/admin/editCategory/${c._id}`}>
-                <HiPencilAlt size={24}/>
-              </Link>
+            <Removebtn id={c._id} />
+            <Link href={`/admin/editCategory/${c._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
           </div>
         </div>
       ))}
